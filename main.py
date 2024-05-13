ebb5e3b4fa32c7fb3247e2ca916c0f4149a49a9a
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2024-05-13 11:01:16 +0300

    Remove example

--- main.py
+++ main.py
@@ -1,53 +1,42 @@
-from src.propagator import ExpressionPropagator
-from src.information import create_information_from_list
-from src.rule import Rule
 from src.expression_deductor import ExpressionInformationDeductor
 from src.interval import Interval
 
 import sympy as sym
 
 if __name__ == '__main__':
-    # first_expr_information = create_information_from_list(['strict_convex'])
-    # second_expr_information = create_information_from_list(['strict_convex'])
-
-    # first_propagator = ExpressionPropagator(expression_information=first_expr_information)
-    # second_propagator = ExpressionPropagator(expression_information=second_expr_information)
-    # result = first_propagator.max([first_propagator, second_propagator])
-    # print(result.expression_information.convexity_information.is_strict_convex)
-
     deductor = ExpressionInformationDeductor()
 
     x = sym.Symbol('x')
     
-    # expr = sym.UnevaluatedExpr(sym.Max(2 - x, 4*x - x**2))
-    # info = deductor.get_information(
-    #     expression=expr,
-    #     interval = Interval([0, 2])
-    # )
-    # print(info.expression_information)
-    # print(info.interval)
+    expr = sym.UnevaluatedExpr(sym.Max(2 - x, 4*x - x**2))
+    info = deductor.get_information(
+        expression=expr,
+        interval = Interval([0, 2])
+    )
+    print(info.expression_information)
+    print(info.interval)
 
-    # expr = sym.UnevaluatedExpr(-sym.cos(x) + sym.exp(-x))
-    # info = deductor.get_information(
-    #     expression=expr,
-    #     interval = Interval([0, 1])
-    # )
-    # print(info.expression_information)
-    # print(info.interval)
+    expr = sym.UnevaluatedExpr(-sym.cos(x) + sym.exp(-x))
+    info = deductor.get_information(
+        expression=expr,
+        interval = Interval([0, 1])
+    )
+    print(info.expression_information)
+    print(info.interval)
     
-    # expr = sym.UnevaluatedExpr(sym.Abs(x**3 + x**2 + 4 * x))
-    # info = deductor.get_information(
-    #     expression=expr,
-    #     interval=Interval([-2, 1])
-    # )
-    # print(info.expression_information)
-    # print(info.interval)
+    expr = sym.UnevaluatedExpr(sym.Abs(x**3 + x**2 + 4 * x))
+    info = deductor.get_information(
+        expression=expr,
+        interval=Interval([-2, 1])
+    )
+    print(info.expression_information)
+    print(info.interval)
 
-    # expr = sym.UnevaluatedExpr((x + sym.cos(x))**2)
-    # info = deductor.get_information(
-    #     expression=expr,
-    #     interval=Interval([-1, 1])
-    # )
-    # print(info.expression_information)
-    # print(info.interval)
+    expr = sym.UnevaluatedExpr((x + sym.cos(x))**2)
+    info = deductor.get_information(
+        expression=expr,
+        interval=Interval([-1, 1])
+    )
+    print(info.expression_information)
+    print(info.interval)
     
